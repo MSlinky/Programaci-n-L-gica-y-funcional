1780151c8855fdfed9b081b610cab48464a01062
MSlinky <wwwmario15@hotmail.com>
2016-09-14 19:23:32 -0500

    Add files via upload

---
+++ Practica 1/practica1.pl
@@ -0,0 +1,28 @@
+madre("Benita Gomez","Claudia Zavala").
+padre("Epifanio Zavala","Claudia Zavala").
+madre("Benita Gomez","Vicente Zavala").
+padre("Epifanio Zavala","Vicente Zavala").
+madre("Benita Gomez","Elisa Zavala").
+padre("Epifanio Zavala","Elisa Zavala").
+madre("Benita Gomez","Magdalena Zavala").
+padre("Epifanio Zavala","Magdalena Zavala").
+madre("Benita Gomez","Maria de la luz Zavala").
+padre("Epifanio Zavala","Maria de la luz  Zavala").
+madre("Benita Gomez","Ramona Zavala").
+padre("Epifanio Zavala","Ramona Zavala").
+
+madre("Magdalena Ramirez","Jesus Solano").
+padre("Jesus Solano","Jesus Solano").
+madre("Magdalena Ramirez","Dolores Solano").
+padre("Jesus Solano","Dolores Solano").
+madre("Magdalena Ramirez","Teresa Solano").
+padre("Jesus Solano","Teresa Solano").
+madre("Magdalena Ramirez","Humberto Solano").
+padre("Jesus Solano","Humberto Solano").
+madre("Magdalena Ramirez","Luz Mila Solano").
+padre("Jesus Solano","Luz Mila Solano").
+madre("Magdalena Ramirez","Jorge Solano").
+padre("Jesus Solano","Jorge Solano").
+
+madre("Claudia Zavala","Mario Solano").
+padre("Jesus Solano","Mario Solano").
